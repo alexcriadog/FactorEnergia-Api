9ef0ff2844fb5ffd468369df3ad8991f7d84523b
acriado1 <acriado@gemweb.es>
2023-12-09 23:20:02 +0100

    Dockerizado y endpoint funcional

--- app/Http/Controllers/StackOverflowController.php
+++ app/Http/Controllers/StackOverflowController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use App\Models\StackOverflowModel;
+use App\Models\StackOverflowQuery;
 
 class StackOverflowController extends Controller
 {
@@ -18,6 +18,19 @@ class StackOverflowController extends Controller
         // Ver si contiene los filtros todate y fromdate y guardarlos en el formato que se requiere en la API de StackOverflow
         $toDate = $request->has('todate') ? strtotime($request->input('todate')) : null;
         $fromDate = $request->has('fromdate') ? strtotime($request->input('fromdate')) : null;
+
+        // Verificar si la consulta ya existe en la base de datos
+        $existingQuery = StackOverflowQuery::where('tagged', $request->input('tagged'))
+            ->where('to_date', $toDate)
+            ->where('from_date', $fromDate)
+            ->first();
+
+        // Si la consulta ya existe, devolver los resultados almacenados
+        if ($existingQuery) {
+            $decodedResult = json_decode($existingQuery->result, true);
+            return ['api', response()->json($decodedResult)];
+            return response()->json($decodedResult);
+        }
 
         // Construir la URL de la API Stack Exchange con los filtros
         $apiUrl = 'https://api.stackexchange.com/2.3/questions';
@@ -38,6 +51,14 @@ class StackOverflowController extends Controller
         
         // Realizar la consulta a la API de Stack Overflow
         $apiResponse = Http::get($apiUrl, $apiParams);
+
+        // Almacenar la consulta y sus resultados en la base de datos
+        StackOverflowQuery::create([
+            'tagged' => $request->input('tagged'),
+            'to_date' => $toDate,
+            'from_date' => $fromDate,
+            'result' => json_encode($apiResponse->json()),
+        ]);
 
         return $apiResponse;
     }
--- app/Models/StackOverflowQuery.php
+++ app/Models/StackOverflowQuery.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class StackOverflowModel extends Model
+class StackOverflowQuery extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tagged',  'to_date', 'from_date', 'result'
+    ];
 }
