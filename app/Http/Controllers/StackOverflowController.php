97c4a6f54fbbe3b93324c346e30cdd23d3dd8f88
acriado1 <acriado@gemweb.es>
2023-12-09 14:05:42 +0100

    endpoint creado, con los filtros correspondientes y se comunica con la API de stackoverflow

--- app/Http/Controllers/StackOverflowController.php
+++ app/Http/Controllers/StackOverflowController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\StackOverflowModel;
 
 class StackOverflowController extends Controller
 {
     //
+    public function getDataFromAPI(Request $request){
+        // Validar que el filtro 'tagged' sea obligatorio
+        $request->validate([
+            'tagged' => 'required',
+        ]);
+
+        // Ver si contiene los filtros todate y fromdate y guardarlos en el formato que se requiere en la API de StackOverflow
+        $toDate = $request->has('todate') ? strtotime($request->input('todate')) : null;
+        $fromDate = $request->has('fromdate') ? strtotime($request->input('fromdate')) : null;
+
+        // Construir la URL de la API Stack Exchange con los filtros
+        $apiUrl = 'https://api.stackexchange.com/2.3/questions';
+        $apiParams = [
+            'order' => 'desc',
+            'site' => 'stackoverflow',
+            'tagged' => $request->input('tagged'),
+        ];
+
+        // Agregar 'todate' y 'fromdate' solo si están presentes en la solicitud
+        if ($toDate !== null) {
+            $apiParams['min'] = $toDate;
+        }
+
+        if ($fromDate !== null) {
+            $apiParams['fromdate'] = $fromDate;
+        }
+        
+        // Realizar la consulta a la API de Stack Overflow
+        $apiResponse = Http::get($apiUrl, $apiParams);
+
+        return $apiResponse;
+    }
 }
